44ea2c0490b3efab88752a91588b765c0eb3e0d2
Fanny Jiang <jiangf@bu.edu>
2017-03-11 15:53:58 -0500

    completed set up

---
+++ twitter-js-practice/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express(); // creates an instance of an express application
+
+app.listen(3000, function() {
+  console.log('Connected and server listening!');
+});
+
